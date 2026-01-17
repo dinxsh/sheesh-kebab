6f31922b3414bd603b34323c036793c94a8980ea
dinxsh <talwadkerdinesh@gmail.com>
2026-01-18 01:33:53 +0530

    chore: tests + docs v1

---
+++ services/agriculture/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { AgricultureModule } from './agriculture/agriculture.module';
+
+@Module({
+  imports: [AgricultureModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
